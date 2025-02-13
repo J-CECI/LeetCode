8f4b2031bf6384a7d5329fa3081b33d4f55f8630
Joana Ceci <136910720+J-CECI@users.noreply.github.com>
2025-02-13 20:50:05 +0200

    Time: 1641 ms (37.88%), Space: 0B (100%) - LeetHub

---
+++ 1958-ad-free-sessions/1958-ad-free-sessions.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+select p.session_id 
+from Playback p
+where (session_id) not in (
+    select p.session_id 
+    from Playback p
+    left join Ads a
+    on p.customer_id = a.customer_id
+    where a.timestamp between p.start_time and p.end_time 
+);
