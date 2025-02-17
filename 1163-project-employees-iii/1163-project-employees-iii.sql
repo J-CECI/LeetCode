ccc19185c6fc08d42925fac433bae91ab5704943
Joana Ceci <136910720+J-CECI@users.noreply.github.com>
2025-02-17 13:17:04 +0200

    Time: 1475 ms (14.28%), Space: 0B (100%) - LeetHub

---
+++ 1163-project-employees-iii/1163-project-employees-iii.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+select project_id,
+       employee_id
+from (
+select p.project_id,
+       p.employee_id,
+       rank() over (partition by project_id order by experience_years desc ) as  rnk
+from Project p
+join Employee e on p.employee_id = e.employee_id 
+) a 
+where rnk =1 ;
