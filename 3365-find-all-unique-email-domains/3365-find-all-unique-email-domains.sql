7ffc6d92499e9cc80abe7c27e3e3aa3d3236189f
Joana Ceci <136910720+J-CECI@users.noreply.github.com>
2025-09-06 21:12:16 +0300

    Time: 254 ms (98.63%), Space: 0B (100%) - LeetHub

---
+++ 3365-find-all-unique-email-domains/3365-find-all-unique-email-domains.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select substring_index(email,'@',-1) as email_domain,
+       count(distinct id) as count
+from Emails
+where email like '%.com'
+group by email_domain
+order by email_domain asc;
